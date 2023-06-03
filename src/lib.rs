cd4f7809b143603aca2a3fd8c4aee634947a95b1
Armin Sander <armin@replicator.org>
2023-06-03 22:22:54 +0200

    Create a pathological test case in which a var is droped after being accessed in a computation

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub use var::Var;
 
 #[cfg(test)]
 mod tests {
+    use std::cell::RefCell;
+
     use crate::engine::Engine;
 
     #[test]
@@ -23,5 +25,27 @@ mod tests {
         assert_eq!(*c.get(), 3);
         b.set(3);
         assert_eq!(*c.get(), 4);
+    }
+
+    /// This wraps the `a` variable in a `RefCell` and drops it in the computation, even though it
+    /// was read from and recorded as a dependency.
+    #[test]
+    fn pathological() {
+        let engine = Engine::new();
+        let a = engine.var(1);
+        let b = engine.var(2);
+
+        let c = {
+            let b = b.share();
+            let a = RefCell::new(a);
+            let e = engine.clone();
+            engine.computed(move || {
+                let r = *a.borrow().get() + *b.get();
+                // force a drop of a, even though it has readers.
+                *a.borrow_mut() = e.var(1);
+                r
+            })
+        };
+        assert_eq!(*c.get(), 3);
     }
 }
